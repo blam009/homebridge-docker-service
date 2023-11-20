531107aad291480d54d92f5c9d08858f421683cb
Billy Lam <billy.lam@garmin.com>
2023-11-19 18:06:01 -0800

    Network based installation

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ help() {
     echo "       $0 --help"
     echo
     echo "OPTIONS:"
-    echo "  -H,--homebridge <HOMEBRIDGE>    Homebridge stoarge path. Default is"
-    echo "                                  /var/lib/homebridge."
-    echo "  -n,--network <NETWORK>          Docker network to use. Default is host."
-    echo "  -u,--uninstall                  Uninstall the files and reload the system."
-    echo "  -h,--help                       Print this help menu."
+    echo "  -H,--homebridge <HOMEBRIDGE>        Homebridge stoarge path. Default is"
+    echo "                                      /var/lib/homebridge."
+    echo "  -n,--network <host|novpn-docker>    Docker network to use. Default is host."
+    echo "  -u,--uninstall                      Uninstall the files and reload the system."
+    echo "  -h,--help                           Print this help menu."
 }
 
 HOMEBRIDGE="/var/lib/homebridge"
@@ -43,26 +43,63 @@ while true; do
     esac
 done
 
-SYSTEMD="/etc/systemd/system"
-DOCKER_IMAGE="homebridge-docker"
-DOCKER_SERVICE="$DOCKER_IMAGE@.service"
-UNIT_DEST_PATH="$SYSTEMD/$DOCKER_SERVICE"
-if systemctl is-active "$DOCKER_IMAGE@*" --all --quiet; then
-    echo "Stopping $DOCKER_SERVICE..."
-    sudo systemctl stop "$DOCKER_IMAGE@*" --all
+if [[ "$NETWORK" != "host" && "$NETWORK" != "novpn-docker" ]]; then
+    echo "Error: Invalid network set: $NETWORK"
+    help
+    exit 1
 fi
-if [ -f "$UNIT_DEST_PATH" ]; then
-    echo "Removing $DOCKER_SERVICE..."
-    sudo rm -rf "$UNIT_DEST_PATH"
+
+if [[ ! -d "$HOMEBRIDGE" ]]; then
+    echo "Error: Invalid homebridge directory: $HOMEBRIDGE"
+    help
+    exit 1
+fi
+
+SYSTEMD="/etc/systemd/system"
+IMAGE="homebridge-docker"
+SERVICE="$IMAGE.service"
+SERVICE_DEST_PATH="$SYSTEMD/$SERVICE"
+if systemctl is-active "$SERVICE" --quiet; then
+    echo "Stopping $SERVICE..."
+    sudo systemctl stop "$SERVICE"
+fi
+if [[ "$(systemctl is-enabled "$SERVICE")" = "enabled" ]]; then
+    echo "Disabling $SERVICE..."
+    sudo systemctl disable "$SERVICE"
+fi
+if [ -f "$SERVICE_DEST_PATH" ]; then
+    echo "Removing $SERVICE..."
+    sudo rm -rf "$SERVICE_DEST_PATH"
 fi
 if ! "$UNINSTALL"; then
     echo "Pulling latest homebridge docker image..."
     docker pull homebridge/homebridge:latest
-    echo "Installing $DOCKER_SERVICE..."
-    sed "s#{{HOMEBRIDGE}}#$HOMEBRIDGE#g;s#{{NETWORK}}#$NETWORK#g" \
-        "$DOCKER_SERVICE" | sudo tee "$UNIT_DEST_PATH" >/dev/null
+    echo "Installing $SERVICE..."
+
+    SERVICE_TMP_PATH="/tmp/$SERVICE"
+    cp "$SERVICE" "$SERVICE_TMP_PATH"
+
+    echo "...Using homebridge path: $HOMEBRIDGE"
+    sed "s#{{HOMEBRIDGE}}#$HOMEBRIDGE#g" -i "$SERVICE_TMP_PATH"
+
+    echo "...Using network: $NETWORK"
+    sed "s#{{NETWORK}}#$NETWORK#g" -i "$SERVICE_TMP_PATH"
+
+    if [[ "$NETWORK" == "host" ]]; then
+        sed "/NOVPN/d" -i "$SERVICE_TMP_PATH"
+        printf "\n[Install]\nWantedBy=multi-user.target\n" >> "$SERVICE_TMP_PATH"
+    elif [[ "$NETWORK" == "novpn-docker" ]]; then
+        sed "s|{{NOVPN_RUNTIME_ENV}}|/var/run/novpn/env|g" -i "$SERVICE_TMP_PATH"
+    fi
+
+    sudo install -m644 "$SERVICE_TMP_PATH" "$SERVICE_DEST_PATH"
 fi
 
 sudo systemctl daemon-reload
+
+if ! "$UNINSTALL" && grep -q '\[Install\]' "$SERVICE_DEST_PATH"; then
+    echo "Enabling $SERVICE..."
+    sudo systemctl enable "$SERVICE"
+fi
 
 echo "...Done!"
